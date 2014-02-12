dae3d134f1790aaec49315958a2c8cbd9bf3d1cd
Julian Vetter <julian@sec.t-labs.tu-berlin.de>
2014-02-13 00:05:30 +0100

    Implemented the first few load instructions

--- src/z80.c
+++ src/z80.c
@@ -18,8 +18,10 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
+#include <stdio.h>
 #include <stdint.h>
 
+#include "../include/encodings.h"
 #include "../include/z80.h"
 
 /***
@@ -35,8 +37,8 @@ struct Z80_State {
     uint16_t iy;    /* Index Register */
     uint16_t sp;    /* Stack Pointer */
     uint16_t pc;    /* Program Counter */
-    uint8_t ram[8192];  /* 8KB RAM */
-    uint8_t vram[16384]; /* 16KB VRAM */
+    uint8_t ram[RAM_SZ];  /* 8KB RAM */
+    uint8_t vram[VRAM_SZ]; /* 16KB VRAM */
 } z80_state;
 
 void z80_emulate_cycle(void) {
@@ -58,18 +60,118 @@ void z80_init(void) {
     return;
 }
 
+int z80_gp_valid(uint8_t reg) {
+    if ((reg == 0x6) || (reg > 0x7)) {
+        return 0;
+    }
+    return 1;
+}
+
+uint8_t z80_get_t_reg(uint8_t operand) {
+    uint8_t t_reg = (operand & 0x38) >> 3;
+    if (!z80_gp_valid(t_reg)) {
+        printf("Unkown target register %u\n", t_reg);
+        return 0;
+    }
+    return t_reg;
+}
+
+uint8_t z80_get_s_reg(uint8_t operand) {
+    uint8_t s_reg = (operand & 0x7);
+    if (!z80_gp_valid(s_reg)) {
+        printf("Unkown source register %u\n", s_reg);
+        return 0;
+    }
+    return s_reg;
+}
+
+int z80_ram_valid(uint8_t value) {
+    if (value >= RAM_SZ) {
+        return 0;
+    }
+    return 1;
+}
+
+uint8_t z80_fetch_byte(void) {
+    /* Check if PC points to valid position */
+    if (!z80_ram_valid(z80_state.pc)) {
+        printf("Unkown PC position %u\n", z80_state.pc);
+        return 0;
+    }
+    /* Increment PC after returning instruction */
+    return z80_state.pc++;
+}
 
 /***
  * The Z80 CPU can execute 158 different instruction types including all 78 of
  * the 8080A CPU
  */
 void z80_decode_inst() {
+    uint8_t operand_1, operand_2, operand_3;
+    uint8_t s_reg, t_reg;
 
     switch(z80_state.pc) {
-        /* Load and Exchanged */
-        case 0xDD:
+    /* Load and Exchange */
+    case 0xDD:
+        operand_1 = z80_fetch_byte();
+
+        /* LD r, r' */
+        if ((operand_1 & 0xC0) == 0x40) {
+            /* Get source and destination registers */
+            s_reg = z80_get_s_reg(operand_1);
+            t_reg = z80_get_t_reg(operand_1);
+
+            /* Store value of source register in target register*/
+            z80_state.gp[t_reg] = z80_state.gp[s_reg];
+
+        /* LD r, n */
+        } else if ((operand_1 & 0xC7) == 0x06) {
+            /* Load second operand from memory */
+            operand_2 = z80_fetch_byte();
+
+            /* Determine target register */
+            t_reg = z80_get_t_reg(operand_1);
+
+            /* Store operand_2 in target register */
+            z80_state.gp[t_reg] = operand_2;
+
+        /* LD r, (HL) */
+        } else if ((operand_1 & 0xC7) == 0x46) {
+
+            t_reg = z80_get_t_reg(operand_1);
+
+            operand_2 = z80_fetch_byte();
+            if (!z80_ram_valid(operand_2)) {
+                printf("Unkown source RAM address %u for LD instruction\n",
+                        operand_2);
+            }
+            z80_state.gp[t_reg] = z80_state.ram[operand_2];
+
+        /* LD r, (IX+d) */
+        } else if (operand_1 == 0xDD) {
+            operand_2 = z80_fetch_byte();
+            t_reg = z80_get_t_reg(operand_2);
+            operand_3 = z80_fetch_byte();
+
+            z80_state.gp[t_reg] = (z80_state.ix + operand_3);
+
+        /* LD r, (IY+d) */
+        } else if (operand_1 == 0xFD) {
+            operand_2 = z80_fetch_byte();
+            t_reg = z80_get_t_reg(operand_2);
+            operand_3 = z80_fetch_byte();
+
+            z80_state.gp[t_reg] = (z80_state.iy + operand_3);
+
+        /* LD (HL), r */
+        } else if((operand_1 & 0xF8) == 0x70) {
+        }
+
         break;
-        
+    case 0x0:
+        break;
+    }
+
         /* Block Transfer and Search */
         /* Arithmetic and Logical */
         /* Rotate and Shift */
@@ -77,7 +179,5 @@ void z80_decode_inst() {
         /* Jump, Call, and Return */
         /* Input/Output */
         /* Basic CPU Control */
-    }
-    
 }
 
