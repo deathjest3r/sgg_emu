4a80634502ff904736f801dcac8bd0873fd56d0b
Julian Vetter <julian@sec.t-labs.tu-berlin.de>
2014-02-13 00:02:27 +0100

    Added a file that contains the defines for the general purpose registers

---
+++ include/encodings.h
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the SGGEmu project.
+ *
+ * Copyright (C) 2014 Julian Vetter <julian@sec.t-labs.tu-berlin.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef __ENCODING_H__
+#define __ENCODING_H__
+
+#define A 0x7 /*111*/
+#define B 0x0 /*000*/
+#define C 0x1 /*001*/
+#define D 0x2 /*010*/
+#define E 0x3 /*011*/
+#define H 0x4 /*100*/
+#define L 0x5 /*101*/
+
+#endif /*__ENCODING_H__*/
