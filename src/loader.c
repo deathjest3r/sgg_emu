8309f4c1bc260fda2f6e002de8b212cf1b75832d
Julian Vetter <julian@sec.t-labs.tu-berlin.de>
2017-06-29 17:03:45 +0200

    * Parse the Cardridge header

--- src/loader.c
+++ src/loader.c
@@ -20,25 +20,102 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "../include/loader.h"
 
+const uint8_t SEGA_STRING[] = { 0x54, 0x4d, 0x52, 0x20, 0x53, 0x45, 0x47, 0x41, 0x00 };
+const uint16_t HEADER_LOC[] = { 0x1ff0, 0x3ff0, 0x7ff0 };
+
+const char* REGION[] = { "UNKNOWN", "UNKNOWN", "UNKNOWN", "SMS Japan",
+    "SMS Export", "GG Japan", "GG Export", "GG International" };
+
+void rom_size(uint8_t index) {
+  char rs[20];
+  switch(index) {
+  case 0x0a:
+    strncpy(rs, "8KB (unused)", 20);
+    break;
+  case 0x0b:
+    strncpy(rs, "16KB (unused)", 20);
+    break;
+  case 0x0c:
+    strncpy(rs, "32KB", 20);
+    break;
+  case 0x0d:
+    strncpy(rs, "48KB (unused, buggy)", 20);
+    break;
+  case 0x0e:
+    strncpy(rs, "64KB (rarely used)", 20);
+    break;
+  case 0x0f:
+    strncpy(rs, "128KB", 20);
+    break;
+  case 0x00:
+    strncpy(rs, "256KB", 20);
+    break;
+  case 0x01:
+    strncpy(rs, "512KB (rarely used)", 20);
+    break;
+  case 0x2:
+    strncpy(rs, "1MB (unused, buggy)", 20);
+    break;
+  }
+  printf("ROM Size:\t%s\n", rs);
+}
 
 uint8_t* loader_load_rom(const char* path, uint8_t* rom_buffer) {
-    size_t blocks = 0;
+  size_t blocks = 0;
+  uint16_t i;
+  uint8_t region;
 
-    FILE* rom_fd = fopen(path, "r");
-    if (rom_fd == NULL) {
-        printf("Could not open file: %s\n", path);
-        return NULL;
+  FILE* rom_fd = fopen(path, "r");
+  if (rom_fd == NULL) {
+    printf("Could not open file: %s\n", path);
+    return NULL;
+  }
+
+  blocks = fread(rom_buffer, sizeof(uint8_t), 512 * 1024, rom_fd);
+  if (blocks < 512 * 1024) {
+    printf("Could only read %u Bytes\n", (unsigned int)blocks);
+    return NULL;
+  }
+
+  printf("Loaded rom from %s\n", path);
+  /*TODO: Determine right size of loop using sizeof*/
+  for(i = 0; i < 3; i++) {
+    if(strncmp((const char *)SEGA_STRING, (const char *)&rom_buffer[HEADER_LOC[i]], sizeof(SEGA_STRING)) == 0) {
+      printf("Found header @0x%x\n", HEADER_LOC[i]);
+      printf("--------------------\n");
+      printf("SEGA String:\t%02x %02x %02x %02x %02x %02x %02x %02x (%s)\n",
+          rom_buffer[HEADER_LOC[i]],
+          rom_buffer[HEADER_LOC[i] + 1],
+          rom_buffer[HEADER_LOC[i] + 2],
+          rom_buffer[HEADER_LOC[i] + 3],
+          rom_buffer[HEADER_LOC[i] + 4],
+          rom_buffer[HEADER_LOC[i] + 5],
+          rom_buffer[HEADER_LOC[i] + 6],
+          rom_buffer[HEADER_LOC[i] + 7],
+          &rom_buffer[HEADER_LOC[i]]);
+      printf("Reserved:\t%02x %02x\n",
+          rom_buffer[HEADER_LOC[i] + 8],
+          rom_buffer[HEADER_LOC[i] + 9]);
+      printf("Checksum:\t%02x %02x\n",
+          rom_buffer[HEADER_LOC[i] + 10],
+          rom_buffer[HEADER_LOC[i] + 11]);
+      printf("Product Code:\t%02x %02x %02x\n",
+          rom_buffer[HEADER_LOC[i] + 12],
+          rom_buffer[HEADER_LOC[i] + 13],
+          rom_buffer[HEADER_LOC[i] + 14] & 0xf0);
+      printf("Version:\t%02x\n",
+          rom_buffer[HEADER_LOC[i] + 14] & 0x0f);
+      region = (rom_buffer[HEADER_LOC[i] + 15] & 0xf0) >> 4;
+      printf("Region code:\t%02x (%s)\n", region, REGION[region]);
+      rom_size((rom_buffer[HEADER_LOC[i] + 15] & 0x0f));
+      break;
+    } else {
+      printf("Could not find header @0x%x\n", HEADER_LOC[i]);
     }
-
-    blocks = fread(&rom_buffer, sizeof(uint8_t), 512, rom_fd);
-    if (blocks < 512) {
-        printf("Could only read %u Bytes\n", (unsigned int)blocks);
-        return NULL;
-    }
-
-    printf("%s\n", path);
-    return rom_buffer;
+  }
+  return rom_buffer;
 }
