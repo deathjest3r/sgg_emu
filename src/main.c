441d282501e8c0502ec13e6a77fd37a4562c55a0
Julian Vetter <julian@sec.t-labs.tu-berlin.de>
2017-06-30 09:56:34 +0200

    * Updated formatting

--- src/main.c
+++ src/main.c
@@ -33,51 +33,48 @@ extern SDL_Window *G_window;
 extern SDL_Renderer *G_renderer;
 
 static void show_help(char* app_name) {
-    printf("%s -r <rom file>\n", app_name);
+  printf("%s -r <rom file>\n", app_name);
 }
 
 int main(int argc, char* argv[]) {
-    SDL_Event e;
-    int c;
-    bool quit = false;
-    const char* rom_path = "rom/mega_man.gg";
-    uint8_t* rom_ptr = NULL;
+  SDL_Event e;
+  int c;
+  bool quit = false;
+  const char* rom_path = "rom/mega_man.gg";
 
-    while ((c = getopt(argc, argv, "h?")) != -1) {
-        switch (c) {
-        case 'h':
-        case '?':
-        default:
-            show_help(argv[0]);
-        }
+  (void)argc;
+  (void)argv;
+
+  while ((c = getopt(argc, argv, "h?r")) != -1) {
+    switch (c) {
+    case 'h':
+    case '?':
+    case 'r':
+    default:
+      show_help(argv[0]);
+      return 0;
     }
-    rom_ptr = malloc(512 * sizeof(uint8_t));
-    rom_ptr = loader_load_rom(rom_path, rom_ptr);
-    if (rom_ptr == NULL) {
-        printf("Could not load rom from %s\n", rom_path);
-        /*return -1;*/
+  }
+
+  /* Setup system state */
+  z80_init(rom_path);
+  /* Init graphic subsystem of Game Gear */
+  gg_graphics_init();
+
+#ifdef DEBUG
+  printf("\nStaring main emulation loop\n\n");
+#endif
+  while(!quit) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT)
+        quit = true;
+      /*if (e.type == SDL_KEYDOWN)
+        quit = true;*/
+      if (e.type == SDL_MOUSEBUTTONDOWN)
+        quit = true;
+      z80_emulate_cycle();
     }
+  }
 
-    /* Setup system state */
-    z80_init();
-    /* Init graphic subsystem of Game Gear */
-    gg_graphics_init();
-
-    while(!quit) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT)
-                quit = true;
-            /*if (e.type == SDL_KEYDOWN)
-                quit = true;*/
-            if (e.type == SDL_MOUSEBUTTONDOWN)
-                quit = true;
-        }
-    }
-
-    /* Main system loop */
-    /*while(1) {
-        z80_emulate_cycle();
-    }*/
-
-    return 0;
+  return 0;
 }
